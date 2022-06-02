84a1ecb8d2844a9d2e8460b88d312fa1291de865
tbeimrohr <99150183+tbeimrohr@users.noreply.github.com>
2022-06-02 14:06:33 -0700

    Add files via upload

--- Pareto.m
+++ Pareto.m
@@ -23,5 +23,21 @@ mp_factor = mp/mass_tot;
 maxQ_factor = maxQ/maxQ_ref;
 
 L_D = ((mp*2.205/V_loading)/rho)/(pi*(((D1+D2)/4)^2))/((D1+D2)/2);
-L_D_factor = 1 - ((L_D_ref - L_D)/L_D)^2;
+L_D_factor = 1 - (abs((L_D_ref - L_D)/L_D))^2;
+% 
+% tb_factor = (abs((tb_ref - (tb1+tb2))/(tb1+tb2)))/tb_ref;
+% mpl_factor = (abs((mpl_ref - (mpl))/(mpl)))/mpl_ref;
+% alt_factor = (abs((alt_ref - (alt))/(alt)))/alt_ref;
+% mp_factor = (abs((mass_tot - (mp))/(mp)))/mass_tot;
+% maxQ_factor = (abs((maxQ_ref - (maxQ))/(maxQ)))/maxQ_ref;
+% 
+% L_D_factor = 1 - ((abs((L_D_ref - L_D)/L_D))/L_D_ref)^.25
+% 
+% tb_factor = 1 - ((abs((tb_ref - (tb1+tb2))/(tb1+tb2)))/tb_ref)^.25
+% mpl_factor = (abs((mpl_ref - (mpl))/(mpl)))/mpl_ref
+% alt_factor = 1 - (abs((alt_ref - (alt))/(alt)))/alt_ref
+% mp_factor = 1 - (abs((mass_tot - (mp))/(mp)))/mass_tot
+% maxQ_factor = 1 - (abs((maxQ_ref - (maxQ))/(maxQ)))/maxQ_ref
+
+
 
