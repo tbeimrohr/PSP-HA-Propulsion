6e7c54628f2e133a78e7627ee4277a5dc8574e00
tbeimrohr <99150183+tbeimrohr@users.noreply.github.com>
2022-06-07 17:05:30 -0700

    Keeps leading zeros and outputs into a single vector

--- Pareto_write.m
+++ Pareto_write.m
@@ -9,6 +9,7 @@ function Pareto_write
 %
 % Author(s):
 % Justin Kruse
+% Thomas Beimrohr
 % 
 %
 % Description:
@@ -27,28 +28,22 @@ function Pareto_write
 % Notes:
 % YOU WILL NEED 'DEEP LEARNING TOOLBOX' TO USE THIS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc
+clear
 
+payload = [-1,-2,-3]; %Codes for payload mass
+D_1 = [-4,-5,-6]; %Codes for first-stage diameter 
+D_2 = [-7,-8]; %Codes for second-stage diameter
+alt = [-9,10,11]; %Codes for desired altitude
+dv_1 = [12,13,14,15,16]; %Codes for dv % split for stage 1
+coast_lim = [17,18,19,20,21]; %Codes for coasting upper limit
 
-payload = [-1, -2, -3]; %Codes for payload mass
-D_1 = [-4, -5, -6]; %Codes for first-stage diameter 
-D_2 = [-7, -8]; %Codes for second-stage diameter
-alt = [-9, 10, 11]; %Codes for desired altitude
-%dv_1 = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]; %Codes for dv % split for stage 1
-%coast_lim = [23, 24, 25, 26, 27]; %Codes for coasting upper limit
+x = (combvec(payload, D_1, D_2, alt,dv_1,coast_lim))'; % get all possible pareto combinations
 
-x = (combvec(payload, D_1, D_2, alt));% dv_1, coast_lim)); % get all possible pareto combinations
+for i = 1:length(x)
+    combo(i,:) = num2str(x(i,:));
+    temp = replace(combo(i,:), '-', '0');
+    Combinations(i,:) = temp(find(~isspace(temp)));
+end
 
-
-writematrix(x, 'combos.txt') %write to file
-
-%% For exporting with zeroes (possibly useful in the future)%% 
-% for r = 1:length(x(1,:))
-%    for y = 1:length(x(:,1))
-%       combos{y,r} = num2str(x(y,r));
-%    end
-% end
-% combos = replace(combos, '-', '0');
-% writecell(combos)
-%% ---- %%
-
-
+writematrix(Combinations, 'combos.txt') %write to file
